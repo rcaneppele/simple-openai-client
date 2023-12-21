c3d879525fe1bc5143a69b8a0beff9b5e795ca5c
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-12-21 17:04:14 -0300

    methods to get only the message content of a ChatCompletionResponse

--- src/main/java/br/com/rcaneppele/openai/chatcompletion/response/ChatCompletionResponse.java
+++ src/main/java/br/com/rcaneppele/openai/chatcompletion/response/ChatCompletionResponse.java
@@ -16,4 +16,28 @@ public record ChatCompletionResponse(
         List<ChatCompletionChoice> choices,
         TokenUsage usage
 ) {
+    public String firstChoiceMessageContent() {
+        return getMessageContent(0);
+    }
+
+    public String lastChoiceMessageContent() {
+        if (this.choices == null || this.choices.isEmpty()) {
+            return null;
+        }
+
+        return getMessageContent(this.choices.size() - 1);
+    }
+
+    public String messageContentOfChoice(int choice) {
+        return getMessageContent(choice);
+    }
+
+    private String getMessageContent(int choice) {
+        if (this.choices == null || this.choices.isEmpty()) {
+            return null;
+        }
+
+        return this.choices.get(choice).message().content();
+    }
+
 }
